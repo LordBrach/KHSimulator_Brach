e47eab37e0aff5e0ebc92988517d7368e88edaf0
LordBrach <alessandrotano@outlook.fr>
2024-01-08 15:23:44 +0100

    Moveable player

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -14,6 +14,8 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] InputActionReference _move;
     [SerializeField] float _speed;
 
+    private Rigidbody _rBody;
+
     // Event pour les dev
     public event Action OnStartMove;
     public event Action<int> OnHealthUpdate;
@@ -23,16 +25,55 @@ public class PlayerMove : MonoBehaviour
     [SerializeField] UnityEvent _onEventPost;
 
     public Vector2 JoystickDirection { get; private set; }
-    Coroutine MovementRoutine { get; set; }
+    //Coroutine MovementRoutine { get; set; }
 
     private void Start()
     {
-        
+        _rBody = GetComponent<Rigidbody>();
+        if (_rBody == null)
+            Debug.LogError("Player Rigidbody is NULL");
+
+        _move.action.actionMap.Enable();
+
+        _move.action.started += StartMove;
+        _move.action.performed += UpdateMove;
+        _move.action.canceled += StopMove;
     }
 
+    private void UpdateMove(InputAction.CallbackContext context)
+    {
+        //throw new NotImplementedException();
+    }
+
+    private void StopMove(InputAction.CallbackContext context)
+    {
+        //throw new NotImplementedException();
+    }
+
+    private void StartMove(InputAction.CallbackContext context)
+    {
+        OnStartMove?.Invoke();
+        StartCoroutine(PlayerMoveRoutine());
+    }
+
+    IEnumerator PlayerMoveRoutine()
+    {
+        while(true)
+        {
+            JoystickDirection = _move.action.ReadValue<Vector2>();
+            Vector3 direction = new Vector3(JoystickDirection.x, 0, JoystickDirection.y);
+
+            _rBody.velocity = direction * _speed;
+            yield return null;
+        }
+    }
     private void OnDestroy()
     {
-        
+        _move.action.actionMap.Disable();
+
+        _move.action.started -= StartMove;
+        _move.action.performed -= UpdateMove;
+        _move.action.canceled -= StopMove;
     }
 
 }
